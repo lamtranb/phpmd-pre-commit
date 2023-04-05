2b846b44af908cdb596ac4b3904286d88c9d5df8
lamtranb <ltb58@open.ac.uk>
2023-04-05 11:17:29 +0700

    update

--- src/Test.php
+++ src/Test.php
@@ -1,56 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace App;
 
 class Test
 {
+    /**
+     * @var Interface1
+     */
     private $test1;
-    private $test2;
-    private $test3;
-    private $test4;
-    private $test5;
-    private $test6;
-    private $test7;
-    private $test8;
-    private $test9;
-    private $test10;
 
-    public function __construct($test1, $test2, $test3, $test4)
+    /**
+     * @var Interface2
+     */
+    private $test2;
+
+    /**
+     * @var Interface3
+     */
+    private $test3;
+
+    /**
+     * @var Interface4
+     */
+    private $test4;
+
+    public function __construct(Interface1 $test1, Interface2 $test2, Interface3 $test3, Interface4 $test4)
     {
         $this->test1 = $test1;
         $this->test2 = $test2;
         $this->test3 = $test3;
         $this->test4 = $test4;
-    }
-
-    public function setTest5($test5)
-    {
-        $this->test5 = $test5;
-    }
-
-    public function setTest6($test6)
-    {
-        $this->test6 = $test6;
-    }
-
-    public function setTest7($test7)
-    {
-        $this->test7 = $test7;
-    }
-
-    public function setTest8($test8)
-    {
-        $this->test8 = $test8;
-    }
-
-    public function setTest9($test9)
-    {
-        $this->test9 = $test9;
-    }
-
-    public function setTest10($test10)
-    {
-        $this->test10 = $test10;
     }
 
     public function getData()
@@ -60,19 +49,16 @@ class Test
             $this->test2,
             $this->test3,
             $this->test4,
-            $this->test5,
-            $this->test6,
-            $this->test7,
-            $this->test8,
-            $this->test9,
-            $this->test10,
         ];
     }
 
     /**
-     * @param  string   $string1
-     * @param  string   $string2
-     * @return int|null
+     * @param string $string1
+     * @param string $string2
+     * @param mixed  $a
+     * @param mixed  $b
+     *
+     * @return null|int
      */
     public function abc($a, $b): ?string
     {
@@ -89,17 +75,17 @@ class Test
             'test',
             'test',
         ];
-        if (function_exists('normalizer_normalize')) {
+        if (\function_exists('normalizer_normalize')) {
             $a = normalizer_normalize($a);
         }
 
         if (null === $a) {
             $a = '1';
         }
-        if ($a == '1') {
+        if ('1' === $a) {
             return '1';
-        } else {
-            return '2';
         }
+
+        return '2';
     }
 }
